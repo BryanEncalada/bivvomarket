bff18bb54afeccc7cb0a73a3c70ca83fd0e65d0d
BryanEncalada <bryanencalada@gmail.com>
2025-07-07 23:11:05 -0500

    Video-Recetas

--- bivvomarket/src/app/home/home-two/home-two.component.ts
+++ bivvomarket/src/app/home/home-two/home-two.component.ts
@@ -1,33 +1,44 @@
 import { IProduct } from '../../shared/types/IProduct';
 import { ProductService } from '../../shared/services/product.service';
-import { HeroSliderTwoComponent } from "../../shared/components/slider/hero-slider-two/hero-slider-two.component";
-import { HeaderTwoComponent } from "../../shared/header/header-two/header-two.component";
-import { ShopCategoryComponent } from "../../shop/shop-category/shop-category.component";
+import { HeroSliderTwoComponent } from '../../shared/components/slider/hero-slider-two/hero-slider-two.component';
+import { HeaderTwoComponent } from '../../shared/header/header-two/header-two.component';
+import { ShopCategoryComponent } from '../../shop/shop-category/shop-category.component';
 
 import { isPlatformBrowser } from '@angular/common';
 import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
-import { ShopAreaComponent } from "../../shop/components/shop-area/shop-area.component";
+import { ShopAreaComponent } from '../../shop/components/shop-area/shop-area.component';
+import { VideoComponent } from '../../shared/components/video/video.component';
+import { RecetasComponent } from '../../shared/components/recetas/recetas.component';
 
 @Component({
   selector: 'app-home-two',
-  imports: [HeroSliderTwoComponent, HeaderTwoComponent, ShopCategoryComponent, ShopAreaComponent],
+  imports: [
+    HeroSliderTwoComponent,
+    HeaderTwoComponent,
+    ShopAreaComponent,
+    VideoComponent,
+    RecetasComponent,
+  ],
   templateUrl: './home-two.component.html',
-  styleUrl: './home-two.component.scss'
+  styleUrl: './home-two.component.scss',
 })
 export class HomeTwoComponent {
- public big_item: IProduct | undefined;
+  public big_item: IProduct | undefined;
   public trending_products: IProduct[] = [];
   public discount_products: IProduct[] = [];
 
-  constructor(private productService: ProductService, @Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(
+    private productService: ProductService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
     this.productService.products.subscribe((products) => {
-      this.big_item = products.find(p => p.big_img);
-      this.trending_products = products.filter(p => p.trending).slice(0, 6);
-      this.discount_products = products.filter((p) => p.discount! > 0).slice(0,12);
+      this.big_item = products.find((p) => p.big_img);
+      this.trending_products = products.filter((p) => p.trending).slice(0, 6);
+      this.discount_products = products
+        .filter((p) => p.discount! > 0)
+        .slice(0, 12);
     });
   }
-
-
 
   async ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -49,10 +60,6 @@ export class HomeTwoComponent {
           delay: 3000,
         },
       });
-
-
+    }
   }
-
-  }
-
 }
