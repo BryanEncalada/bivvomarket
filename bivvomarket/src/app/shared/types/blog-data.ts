914589b543c346aa3c05435e6d2fb0f4f4f1a435
BryanEncalada <bryanencalada@gmail.com>
2025-07-09 00:09:44 -0500

    colres

--- bivvomarket/src/app/shared/types/blog-data.ts
+++ bivvomarket/src/app/shared/types/blog-data.ts
@@ -222,40 +222,47 @@ const blog_data: IBlogType[] = [
     blog: 'blog-masonry',
   },
 
+  ///---------
 
-///---------
+  {
+    id: 25,
+    img: '/assets/img/blog/2/receta-BM01.png',
+    title: 'Barras energéticas de quinua',
+    author: 'María López',
+    date: 'Julio 8, 2025',
+    desc: 'Una receta fácil y saludable para preparar barras energéticas usando cereal de quinua, perfectas como snack o para después de entrenar.',
+    blog: 'blog-receta',
+  },
 
-{
-  id: 25,
-   img: '/assets/img/blog/2/receta-BM01.png',
-  title: 'Barras energéticas de quinua',
-  author: 'María López',
-  date: 'Julio 8, 2025',
-  desc: 'Una receta fácil y saludable para preparar barras energéticas usando cereal de quinua, perfectas como snack o para después de entrenar.',
-  blog: 'blog-receta',
-},
+  {
+    id: 26,
+    img: '/assets/img/blog/2/receta-BM01.png',
+    title: 'Desayuno crocante con quinua',
+    author: 'Carlos Ruiz',
+    date: 'Julio 8, 2025',
+    desc: 'Aprende a preparar un desayuno crocante y nutritivo con cereal de quinua, frutas y yogur. Ideal para comenzar el día con energía.',
+    blog: 'blog-receta',
+  },
 
-{
-  id: 26,
- img: '/assets/img/blog/2/receta-BM01.png',
-  title: 'Desayuno crocante con quinua',
-  author: 'Carlos Ruiz',
-  date: 'Julio 8, 2025',
-  desc: 'Aprende a preparar un desayuno crocante y nutritivo con cereal de quinua, frutas y yogur. Ideal para comenzar el día con energía.',
-  blog: 'blog-receta',
-},
+  {
+    id: 27,
+    img: '/assets/img/blog/2/receta-BM01.png',
+    title: 'Galletas de quinua y plátano',
+    author: 'Ana Gómez',
+    date: 'Julio 8, 2025',
+    desc: 'Deliciosas galletas caseras hechas con cereal de quinua, plátano maduro y chispas de chocolate. Rápidas, sanas y sin harina refinada.',
+    blog: 'blog-receta',
+  },
 
-{
-  id: 27,
- img: '/assets/img/blog/2/receta-BM01.png',
-  title: 'Galletas de quinua y plátano',
-  author: 'Ana Gómez',
-  date: 'Julio 8, 2025',
-  desc: 'Deliciosas galletas caseras hechas con cereal de quinua, plátano maduro y chispas de chocolate. Rápidas, sanas y sin harina refinada.',
-  blog: 'blog-receta',
-}
-
-
+  {
+    id: 28,
+    img: '/assets/img/blog/2/receta-BM01.png',
+    title: 'Galletas de quinua y plátano',
+    author: 'Ana Gómez',
+    date: 'Julio 8, 2025',
+    desc: 'Deliciosas galletas caseras hechas con cereal de quinua, plátano maduro y chispas de chocolate. Rápidas, sanas y sin harina refinada.',
+    blog: 'blog-receta',
+  },
 ];
 
 export default blog_data;
