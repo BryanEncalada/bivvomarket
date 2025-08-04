d9e4fddb7a402a57300337fd6bd4ff5ddabf8499
BryanEncalada <bryanencalada@gmail.com>
2025-08-03 20:16:09 -0500

    ok

--- bivvomarket/src/app/app.routes.ts
+++ bivvomarket/src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
-import { BackToTopComponent } from './shared/components/back-to-top/back-to-top.component';
 import { NgModule } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+
+// Componentes
 import { HomeTwoComponent } from './home/home-two/home-two.component';
 import { DynamicShopDetailsComponent } from './shop/components/dynamic-shop-details/dynamic-shop-details.component';
 import { ContactComponent } from './pages/contact/contact.component';
-
 import { ShopDetailsComponent } from './shop/components/shop-details/shop-details.component';
 import { CartComponent } from './shop/components/cart/cart.component';
 import { CheckoutComponent } from './shop/components/checkout/checkout.component';
-
 import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
 import { PrivacyComponent } from './pages/privacy/privacy.component';
 
@@ -18,45 +17,45 @@ export const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: HomeTwoComponent,
-    title: 'component: HomeTwoComponent',
+    title: 'Inicio | Bivvo Market',
   },
   {
     path: 'shop/shop-details/:id',
     component: DynamicShopDetailsComponent,
-    title: 'Shop Details Page',
+    title: 'Detalle del producto | Bivvo Market',
     data: {
-      prerender: false
-    }
+      prerender: false,
+    },
   },
   {
     path: 'shop/cart',
     component: CartComponent,
-    title: 'Shop Details Page',
+    title: 'Carrito de compras | Bivvo Market',
   },
   {
     path: 'shop/checkout',
     component: CheckoutComponent,
-    title: 'Shop Details Page',
+    title: 'Finalizar compra | Bivvo Market',
   },
   {
     path: 'shop-details',
     component: ShopDetailsComponent,
-    title: 'Shop Details Page',
+    title: 'Catálogo de productos | Bivvo Market',
   },
   {
     path: 'pages/contact',
     component: ContactComponent,
-    title: 'Contacto',
+    title: 'Contáctanos | Bivvo Market',
   },
   {
     path: 'pages/terminos',
     component: TermsAndConditionsComponent,
-    title: 'Terminos y condiciones',
+    title: 'Términos y condiciones | Bivvo Market',
   },
   {
     path: 'pages/privacidad',
     component: PrivacyComponent,
-    title: 'Politica de privacidad',
+    title: 'Política de privacidad | Bivvo Market',
   },
   {
     path: '**',
@@ -73,7 +72,6 @@ export const routes: Routes = [
     }),
   ],
   exports: [RouterModule],
-  declarations: [], // Add ResourcesComponent to declarations
   providers: [provideClientHydration()],
 })
 export class AppRoutingModule {}
