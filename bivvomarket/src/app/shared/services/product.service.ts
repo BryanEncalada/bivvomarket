b885d4c71b9714c7aa9e0153e6f6a5ec688bbada
Franco Encalada <FrancoEncalada120@gmail.com>
2025-08-14 00:42:49 -0500

    ok

--- bivvomarket/src/app/shared/services/product.service.ts
+++ bivvomarket/src/app/shared/services/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
   public filter_offcanvas: boolean = false;
   public pageSize: number = 9;
   // private apiUrl = 'http://localhost:3000/api/products';
-private apiUrl = 'http://app.enervym.com/api/products';
+private apiUrl = 'https://app.enervym.com/api/products';
 
 
 
