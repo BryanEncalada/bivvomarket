6daf8bccf5b466e90db145addc9692c454df264f
BryanEncalada <93891074+BryanEncalada@users.noreply.github.com>
2025-08-21 17:18:53 -0500

    iconos

--- bivvomarket/src/app/shared/components/video/video.component.ts
+++ bivvomarket/src/app/shared/components/video/video.component.ts
@@ -22,7 +22,7 @@ export class VideoComponent {
     },
     {
       title: 'Portable',
-      subtitle: 'Llévalo contigo como un snack',
+      subtitle: 'Llévalo contigo',
       svg: `<svg  xmlns="http://www.w3.org/2000/svg"  stroke="#225e26"  width="54"  height="54"   viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-paper-bag"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 3h8a2 2 0 0 1 2 2v1.82a5 5 0 0 0 .528 2.236l.944 1.888a5 5 0 0 1 .528 2.236v5.82a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-5.82a5 5 0 0 1 .528 -2.236l1.472 -2.944v-3a2 2 0 0 1 2 -2z" /><path d="M14 15m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M6 21a2 2 0 0 0 2 -2v-5.82a5 5 0 0 0 -.528 -2.236l-1.472 -2.944" /><path d="M11 7h2" /></svg>`, // icono tipo "bolsa o barra snack"
     },
     {
@@ -32,7 +32,7 @@ export class VideoComponent {
     },
     {
       title: 'Nutritivo',
-      subtitle: 'Cómelo mezclado en yogur',
+      subtitle: 'Mezclalo con todo',
       svg: `<svg  xmlns="http://www.w3.org/2000/svg"   stroke="#225e26"  width="54"  height="54"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bowl-spoon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 11h16a1 1 0 0 1 1 1v.5c0 1.5 -2.517 5.573 -4 6.5v1a1 1 0 0 1 -1 1h-8a1 1 0 0 1 -1 -1v-1c-1.687 -1.054 -4 -5 -4 -6.5v-.5a1 1 0 0 1 1 -1z" /><path d="M8 7c1.657 0 3 -.895 3 -2s-1.343 -2 -3 -2s-3 .895 -3 2s1.343 2 3 2" /><path d="M11 5h9" /></svg>`, // icono tipo tazón/yogur
     },
   ];
--- bivvomarket/src/app/shop/components/product-details-area/product-details-area.component.ts
+++ bivvomarket/src/app/shop/components/product-details-area/product-details-area.component.ts
@@ -7,7 +7,7 @@ import { IProduct } from '../../../shared/types/IProduct';
   selector: 'app-product-details-area',
   imports: [ProductDetailsUpperComponent, NgFor, NgClass, NgIf],
   templateUrl: './product-details-area.component.html',
-  styleUrl: './product-details-area.component.scss'
+  styleUrl: './product-details-area.component.scss',
 })
 export class ProductDetailsAreaComponent {
   @Input() product: IProduct | undefined;
@@ -16,10 +16,7 @@ export class ProductDetailsAreaComponent {
   public cssAdditional: boolean = false;
 
   onChange() {
-
     this.cssDescription = !this.cssDescription;
     this.cssAdditional = !this.cssAdditional;
-
   }
-
 }
