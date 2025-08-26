1384188b99aa729ee8783773f32a617a2d952f45
BryanEncalada <bryanencalada@gmail.com>
2025-08-25 22:17:00 -0500

    cambiso direcion

--- bivvomarket/src/app/pages/contact/contact.component.ts
+++ bivvomarket/src/app/pages/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BreadcrumbOneComponent } from '../../shared/breadcrumb/breadcrumb-one/breadcrumb-one.component';
 import { ContactFormComponent } from '../../shared/components/forms/contact-form/contact-form.component';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-contact',
-  imports: [BreadcrumbOneComponent, ContactFormComponent],
+  imports: [BreadcrumbOneComponent, ContactFormComponent, TranslocoModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
